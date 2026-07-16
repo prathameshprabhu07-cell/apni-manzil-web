b497e95a628fdb48c47f222ed4f81378ed0c8029
prathamesh <prathameshprabhu07@gmail.com>
2026-07-16 17:20:54 +0530

    Updated short-term storage changes

--- src/pages/ShortTermStorageForm.jsx
+++ src/pages/ShortTermStorageForm.jsx
@@ -11,10 +11,11 @@ const ShortTermStorageForm = () => {
   const [loading, setLoading] = useState(false);
   
   // n8n प्रोडक्शन URL
-  const n8nUrl = "https://apnimanzil.app.n8n.cloud/webhook/364283f9-0af4-4054-aae1-f8f68cda1a10";
+  const n8nUrl = "http://localhost:5678/webhook/2609871b-9719-4510-80b3-9dd3667e6127"
+
   
   const [formData, setFormData] = useState({
-    location: '', storageDuration: '', spaceNeeded: '', 
+    location: '', storageDuration: '', spaceNeeded: '',
     materialType: 'General Goods', otherMaterial: '', 
     mobile: '', additionalNotes: ''
   });
